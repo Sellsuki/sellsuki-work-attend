11042cc14357d644a66e1852b8bd46211e2fd5f9
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2018-05-20 20:28:08 +0700

    Add bulma package

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <nav class="navbar" style="box-shadow: 0px 5px 20px 0 rgba(0, 0, 0, 0.05); display: flex; justify-content: center;" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          <h1 class="title is-2">
+            Sellsuki Work Attend
+          </h1>
+        </a>
+      </div>
+    </nav>
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -18,6 +28,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
