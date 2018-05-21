a93c4a69385506494a9b8f10b02c1733f5be31d0
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2018-05-21 10:16:56 +0700

    Add vuex package

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import Buefy from 'buefy'
 import './assets/css/main.css'
 import 'buefy/lib/buefy.min.css'
 
+Vue.use(Vuex)
 Vue.component(Buefy.Checkbox.name, Buefy.Checkbox)
 Vue.config.productionTip = false
 
