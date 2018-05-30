e2967119e6edd8790a9714c951c575138d661a0b
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2018-05-30 19:09:49 +0700

    Add all account members

--- src/pages/Result.vue
+++ src/pages/Result.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container dp-flex jtf-ct-center">
     <div class="column is-half">
-      <textarea class="textarea" rows="10" :value="fbPost(selectedAccount, unselectedAccount)"></textarea>
+      <textarea class="textarea" rows="30" :value="fbPost(selectedAccount, unselectedAccount)"></textarea>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import { fbPost } from '../utils/template'
+import { fbPost } from '../utils/Template'
 export default {
   name: 'Result',
   computed: {
