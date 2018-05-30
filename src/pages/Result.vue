1e4706d4ad875676b2c9471d9f63df455fe433e6
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2018-05-30 14:27:25 +0700

    Add template fbPost

--- src/pages/Result.vue
+++ src/pages/Result.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="container dp-flex jtf-ct-center">
     <div class="column is-half">
-      <textarea class="textarea" rows="10" :value="accountNameList"></textarea>
+      <textarea class="textarea" rows="10" :value="fbPost()"></textarea>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import { fbPost } from '../utils/template'
 export default {
   name: 'Result',
   computed: {
     ...mapGetters({
       accountNameList: 'getSelectedAccount'
     })
+  },
+  methods: {
+    fbPost
   }
 }
 </script>
