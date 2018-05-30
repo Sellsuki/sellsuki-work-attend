10ed2798a495d057a62a46bed25315810719e01b
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2018-05-30 16:52:22 +0700

    Restructure vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import store from './store'
@@ -10,7 +9,6 @@ import Buefy from 'buefy'
 import './assets/css/main.css'
 import 'buefy/lib/buefy.min.css'
 
-Vue.use(Vuex)
 Vue.component(Buefy.Checkbox.name, Buefy.Checkbox)
 Vue.config.productionTip = false
 
